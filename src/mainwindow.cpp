9a6dc75fd79450c59df1aa05bca1718f983bf06d
Oz <rik20@live.it>
2015-06-14 09:13:28 +0200

    Fixed copyright notes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010 Debugger20
+Copyright (c) 2015 rikyoz <rik20@live.it>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
