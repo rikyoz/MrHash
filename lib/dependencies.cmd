11f0222402ab88450ed5d4f12c929227e129c56b
Oz <rik20@live.it>
2015-11-27 10:24:28 +0100

    Fix download of QtCryptoHash

---
+++ lib/dependencies.cmd
@@ -0,0 +1,4 @@
+curl -fsSL -v -H 'Accept: application/octet-stream' -o qtcryptohash-static.7z https://api.github.com/repos/rikyoz/QtCryptoHash/releases/assets/1073796
+7z x qtcryptohash-static.7z -oqtcryptohash/ -aoa
+curl -fsSL -v -H 'Accept: application/octet-stream' -o qtcryptohash64-static.7z https://api.github.com/repos/rikyoz/QtCryptoHash/releases/assets/1073794  
+7z x qtcryptohash64-static.7z -oqtcryptohash/ -aoa
