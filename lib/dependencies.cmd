fa43c1e33ae0757afd4573430df57fd7b49846b0
Oz <rik20@live.it>
2015-11-27 10:48:58 +0100

    Fix dependencies.cmd

--- lib/dependencies.cmd
+++ lib/dependencies.cmd
@@ -1,4 +1,4 @@
-curl -fsSL -v -H -o qtcryptohash-static.7z https://github.com/rikyoz/QtCryptoHash/releases/download/v0.1.0/QtCryptoHash-v0.1.0-msvc2013-win32-static.7z
+appveyor DownloadFile https://github.com/rikyoz/QtCryptoHash/releases/download/v0.1.0/QtCryptoHash-v0.1.0-msvc2013-win32-static.7z -FileName qtcryptohash-static.7z 
 7z x qtcryptohash-static.7z -oqtcryptohash/ -aoa
-curl -fsSL -v -H -o qtcryptohash64-static.7z https://github.com/rikyoz/QtCryptoHash/releases/download/v0.1.0/QtCryptoHash-v0.1.0-msvc2013-win64-static.7z 
+appveyor DownloadFile https://github.com/rikyoz/QtCryptoHash/releases/download/v0.1.0/QtCryptoHash-v0.1.0-msvc2013-win64-static.7z -FileName qtcryptohash64-static.7z
 7z x qtcryptohash64-static.7z -oqtcryptohash/ -aoa
