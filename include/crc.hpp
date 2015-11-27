36f70429c9d21bdaa1b00759861032e89d6e7ea6
Oz <rik20@live.it>
2015-11-27 08:45:35 +0100

    Fix CRC

--- include/crc.hpp
+++ include/crc.hpp
@@ -9,13 +9,13 @@ using boost::crc_optimal;
 using boost::crc_ccitt_type;
 
 // CRC16-CCITT (X.25) - the same used by qChecksum
-typedef crc_optimal< 16, 0x1021, 0xFFFF, 0xFFFF, true, true > boost_crc16;
+typedef crc_optimal< 16, 0x1021, 0xffff, 0xffff, true, true > boost_crc16;
 
 // CRC-32 (PKZIP) - http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat-bits.32
 typedef crc_optimal< 32, 0x04c11db7, 0xffffffff, 0xffffffff, true, true > boost_crc32;
 
 // CRC-64 (XZ) - http://reveng.sourceforge.net/crc-catalogue/17plus.htm#crc.cat-bits.64
-typedef crc_optimal< 64, 0x42f0e1eba9ea3693, 0xffffffffffffffffULL, 0xffffffffffffffffULL, true, true > boost_crc64;
+typedef crc_optimal< 64, 0x42f0e1eba9ea3693, 0xffffffffffffffff, 0xffffffffffffffff, true, true > boost_crc64;
 
 struct QChecksum {
     enum Algorithm { CRC16, CRC32, CRC64 };
